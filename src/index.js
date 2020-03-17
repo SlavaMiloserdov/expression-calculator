ab22402294d3265451e01409a0241e35b8e7138b
SlavaMiloserdov <slavmiloserdov@mail.ru>
2020-03-17 15:41:49 +0400

    feat: add solution for hard

--- src/index.js
+++ src/index.js
@@ -28,14 +28,13 @@ class ACTION {
 
 function expressionCalculator(expr) {
   try {
-    let stackNumbers = [];
-    let stackActions = [];
-    let startArrayNumbers = expr
+    let result;
+    var startArrayNumbers = expr
       .split(" ")
       .join("")
       .split(/[\\[+*-/?(){|^$]/g)
       .filter(item => item.length > 0);
-    let startArrayActions = expr
+    var startArrayActions = expr
       .split(" ")
       .join("")
       .split(/\d/g)
@@ -54,80 +53,65 @@ function expressionCalculator(expr) {
     if (lengthOpenParentheses !== lengthCloseParentheses)
       throw "ExpressionError: Brackets must be paired";
 
-    let lengthNumbers = startArrayNumbers.length;
-    let lengthActions = startArrayActions.length;
+    result = getNumber(startArrayNumbers, startArrayActions)[0];
 
-    while (1 > 0) {
-      stackNumbers.push(+startArrayNumbers.shift());
-      let action = new ACTION(startArrayActions.shift());
-      let previousAction = getPrevAction(stackActions);
-      let nextAction = getNextAction(startArrayActions);
-      if (action.symbol === undefined) {
-        if (lengthActions > lengthNumbers) stackNumbers.pop();
-        do {
-          let tempNumber = doAction(stackNumbers, stackActions.pop().symbol);
-          stackNumbers.push(tempNumber);
-        } while (stackActions.length !== 0);
-        break;
-      }
-      if (action.symbol === "(") {
-        let tempArrayActions = [];
-        let tempArrayNumbers = [];
-
-        tempArrayNumbers.push(stackNumbers.pop());
-
-        while (getNextAction(startArrayActions) !== ")") {
-          tempArrayActions.push(startArrayActions.shift());
-        }
-        startArrayActions.shift();
-        for (let i = 0; i < tempArrayActions.length; i++) {
-          tempArrayNumbers.push(startArrayNumbers.shift());
-        }
-
-        stackNumbers.push(
-          doActionInParentheses(tempArrayNumbers, tempArrayActions)[0]
-        );
-        if (startArrayActions.length !== 0) {
-          let newAction = new ACTION(startArrayActions.shift());
-          while (
-            getPrevAction(stackActions) !== "undefined" &&
-            getPrevAction(stackActions).priority >= newAction.priority
-          ) {
-            let resultActions = doAction(
-              stackNumbers,
-              stackActions.pop().symbol
-            );
-            stackNumbers.push(resultActions);
-          }
-          stackActions.push(newAction);
-          lengthActions = lengthActions - 1;
-        }
-        continue;
-      }
-      if (
-        previousAction === "undefined" ||
-        action.priority > previousAction.priority
-      ) {
-        stackActions.push(action);
-      } else {
-        while (
-          getPrevAction(stackActions) !== "undefined" &&
-          getPrevAction(stackActions).priority >= action.priority
-        ) {
-          let resultActions = doAction(stackNumbers, stackActions.pop().symbol);
-          stackNumbers.push(resultActions);
-        }
-        stackActions.push(action);
-      }
-    }
-    if (Number(stackNumbers) >= 0 || Number(stackNumbers) < 0) {
-      if (Number(stackNumbers) < -10 || Number(stackNumbers) > 10) {
-        return +Number(stackNumbers).toFixed(4);
-      } else return +stackNumbers;
+    if (Number(result) >= 0 || Number(result) < 0) {
+      if (Number(result) < -10 || Number(result) > 10) {
+        return +Number(result).toFixed(4);
+      } else return +result;
     } else throw new Error();
   } catch (error) {
     throw new Error(error);
   }
+}
+
+function getNumber(startArrayNumbers, startArrayActions) {
+  let stackNumbers = [];
+  let stackActions = [];
+
+  while (1 > 0) {
+    stackNumbers.push(+startArrayNumbers.shift());
+    let action = new ACTION(startArrayActions.shift());
+    let previousAction = getPrevAction(stackActions);
+    if (action.symbol === undefined) {
+      while (stackActions.length !== 0) {
+        let tempNumber = doAction(stackNumbers, stackActions.pop().symbol);
+        stackNumbers.push(tempNumber);
+      }
+      break;
+    }
+    if (action.symbol === "(") {
+      let tempNumber = stackNumbers.pop();
+      startArrayNumbers.unshift(tempNumber);
+      stackNumbers.push(getNumber(startArrayNumbers, startArrayActions)[0]);
+      previousAction = getPrevAction(stackActions);
+      startArrayNumbers.unshift(stackNumbers.pop());
+      continue;
+    }
+    if (action.symbol === ")") {
+      do {
+        let tempNumber = doAction(stackNumbers, stackActions.pop().symbol);
+        stackNumbers.push(tempNumber);
+      } while (stackActions.length !== 0);
+      break;
+    }
+    if (
+      previousAction === "undefined" ||
+      (action.priority > previousAction.priority && action.symbol !== "(")
+    ) {
+      stackActions.push(action);
+    } else {
+      while (
+        getPrevAction(stackActions) !== "undefined" &&
+        getPrevAction(stackActions).priority >= action.priority
+      ) {
+        let resultActions = doAction(stackNumbers, stackActions.pop().symbol);
+        stackNumbers.push(resultActions);
+      }
+      stackActions.push(action);
+    }
+  }
+  return stackNumbers;
 }
 
 function getPrevAction(array) {
@@ -152,40 +136,6 @@ function doAction(array, action) {
       if (secondNumber === 0) throw "TypeError: Division by zero.";
       return firstNumber / secondNumber;
   }
-}
-
-function doActionInParentheses(arrayOfNumbers, arrayOfActions) {
-  let stackNumbers = [];
-  let stackActions = [];
-  while (1 > 0) {
-    stackNumbers.push(+arrayOfNumbers.shift());
-    let action = new ACTION(arrayOfActions.shift());
-    let previousAction = getPrevAction(stackActions);
-    // let nextAction = getNextAction(arrayOfActions);
-    if (action.symbol === undefined) {
-      do {
-        let tempNumber = doAction(stackNumbers, stackActions.pop().symbol);
-        stackNumbers.push(tempNumber);
-      } while (stackActions.length !== 0);
-      break;
-    }
-    if (
-      previousAction === "undefined" ||
-      action.priority > previousAction.priority
-    ) {
-      stackActions.push(action);
-    } else {
-      while (
-        getPrevAction(stackActions) !== "undefined" &&
-        getPrevAction(stackActions).priority >= action.priority
-      ) {
-        let resultActions = doAction(stackNumbers, stackActions.pop().symbol);
-        stackNumbers.push(resultActions);
-      }
-      stackActions.push(action);
-    }
-  }
-  return stackNumbers;
 }
 
 module.exports = {
